71638d7075e6f845ae2449705bdfb0392f3ec05c
Jason Roger Marc Edmonds <pn68ipb@eduvaud.ch>
2026-08-28 12:13:47 +0200

    Add error handling middleware

--- api/app.js
+++ api/app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var errorHandler = require("./middleware/errorHandler");
 
 var app = express();
 
@@ -16,5 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/", indexRouter);
 app.use("/api/users", usersRouter);
+
+app.use(errorHandler);
 
 module.exports = app;
